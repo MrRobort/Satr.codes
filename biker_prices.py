d273c67c7b6e193b8c0e71fe3210dd194b2d0ddb
Mohammed Abdulrahman <mr_robort@icloud.com>
2022-04-03 14:16:37 -0700

    Update biker_prices.py
    More OOP

--- biker_prices.py
+++ biker_prices.py
@@ -1,33 +1,33 @@
-class Bikes:
-    def __init__(self,description, cost, sale_price, condution):
+class Bikes():
+    def __init__(self):
+        self.description = ''
+        self.cost = 0
+        self.sale_price = 0
+        self.condution = 0
+        self.sold = False
+
+    def create(self, description,cost, sale_price, condution):
         self.description = description
-        self.cost = cost
+        self.cost= cost
         self.sale_price = sale_price
         self.condution = condution
-    
-    def create(self):
-        return {
-            'Description': self.description,
-            'Cost': self.cost,
-            'Sale Price': self.sale_price,
-            'Condition': self.condution,
-            'Sold': False
-            }
-    
-    def sell(self,bike):
-        bike['Sold'] = True
         
-    def update_price(self, bike, sale_price):
-        if(bike['Sold'] == True):
-            print("Action Not Allowed, the bike already sold")
+
+    def sell(self,bike):
+        self.sold = True
+
+    def update_price(self,new_price):
+        if (self.sold):
+            print("Action Not allowed, the bike already sold")
         else:
-            bike['Sale Price'] = sale_price
-            
-    
-bike_obj = Bikes('Univerage voge', cost=100, sale_price=350, condution=0.5)
-bike1 = bike_obj.create()
-print(bike1)
-bike_obj.update_price(bike1, 800)
-print(bike1)
-bike_obj.sell(bike1)
-print(bike1)
+            self.sale_price = new_price
+
+    def print_values(self):
+        print("Description: " , self.description , 'Cost: ' , self.cost , 'Sale price: ' , self.sale_price , "Condution: " , self.condution , "Sold: ", self.sold)
+
+bike_obj = Bikes()
+
+bike1 = bike_obj.create('bike', 100, 250, 0.35)
+bike_obj.print_values()
+bike_obj.update_price(500)
+bike_obj.print_values()
